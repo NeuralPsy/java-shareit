d9de54c191086bb656b1911cbe0359839acffd48
NeuralPsy <kirill.neuralpsy@gmail.com>
2022-12-28 20:01:41 +0200

    Add User and Booking Entities

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -5,14 +5,22 @@ import ru.practicum.shareit.booking.status.BookingStatus;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
 public class Booking {
+    @Id
     private int id;
     private LocalDateTime start;
     private LocalDateTime end;
+    @OneToOne
     private Item item;
+    @ManyToOne
     private User booker;
     private BookingStatus status;
 
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,11 +1,22 @@
 package ru.practicum.shareit.user.model;
 
 import lombok.Data;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.model.Item;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Data
+@Entity
 public class User {
+    @Id
     private int id;
     private String name;
     private String email;
+    @OneToMany(mappedBy = "booker")
+    private List<Booking> bookings;
 
 }
