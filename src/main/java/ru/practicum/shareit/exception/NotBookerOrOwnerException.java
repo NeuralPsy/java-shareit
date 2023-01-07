924c119d05fceba474a5e633bcaff3e686537022
NeuralPsy <kirill.neuralpsy@gmail.com>
2023-01-07 21:10:06 +0200

    Fix: clean up the code

--- src/main/java/ru/practicum/shareit/exception/NotBookerOrOwnerException.java
+++ src/main/java/ru/practicum/shareit/exception/NotBookerOrOwnerException.java
@@ -1,7 +1,7 @@
 package ru.practicum.shareit.exception;
 
-public class NotBookerOrOwnerException extends RuntimeException{
-    public NotBookerOrOwnerException(String msg){
+public class NotBookerOrOwnerException extends RuntimeException {
+    public NotBookerOrOwnerException(String msg) {
         super(msg);
     }
 }
